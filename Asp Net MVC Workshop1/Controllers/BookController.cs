1407557bf2647c0003c68892e9815e547877ad33
1105137241 <1105137241@nkust.edu.tw>
2019-05-07 16:37:31 +0800

    1

---
+++ Asp Net MVC Workshop1/Controllers/BookController.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using Asp_Net_MVC_Workshop1.Models;
+
+
+namespace Asp_Net_MVC_Workshop1.Controllers
+{
+    public class BookController : Controller
+    {
+
+        Database1Entities db=new Database1Entities();
+
+        // GET: Book
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+
+
+    }
+}
